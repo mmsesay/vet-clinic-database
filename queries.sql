1f1e81c632083c2531d893874584fa450eec873a
mmsesay <maej_01@hotmail.com>
2021-11-29 14:46:14 +0000

    Add query to find animals that weight more than 10.5kg

--- queries.sql
+++ queries.sql
@@ -4,4 +4,4 @@ SELECT * FROM animals WHERE name LIKE '%mon';
 SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
 SELECT name FROM animals WHERE neutered='true' AND escape_attempt < 3;
 SELECT date_of_birth FROM animals WHERE name='Agumon' OR name='Pikachu';
-
+SELECT name, escape_attempt FROM animals WHERE weight_kg > 10.5;
