27638167818377129a0034f32c9374c47b504ec7
mmsesay <maej_01@hotmail.com>
2021-11-29 14:44:02 +0000

    Add query for find date of birth of either two animals

--- queries.sql
+++ queries.sql
@@ -3,3 +3,5 @@
 SELECT * FROM animals WHERE name LIKE '%mon';
 SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
 SELECT name FROM animals WHERE neutered='true' AND escape_attempt < 3;
+SELECT date_of_birth FROM animals WHERE name='Agumon' OR name='Pikachu';
+
