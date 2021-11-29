11d3d8a96e0463ba33a1fbaf0d4981c24bc790ef
mmsesay <maej_01@hotmail.com>
2021-11-29 13:22:11 +0000

    Add the animals table schema

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+    id integer PRIMARY KEY,
+	name varchar(100),
+	date_of_birth date,
+	escape_attempt integer,
+	neutered boolean,
+	weight_kg decimal(13,2)
 );
