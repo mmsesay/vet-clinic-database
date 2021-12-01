3898409a956dbf4fbea8fb1baec5f24735b08d2e
mmsesay <maej_01@hotmail.com>
2021-12-01 07:52:26 +0000

    Add species column to the table

--- queries.sql
+++ queries.sql
@@ -8,3 +8,5 @@ SELECT name, escape_attempt FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered='true';
 SELECT * FROM animals WHERE name != 'Gabumon';
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+
