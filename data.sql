328f505afe0164463202957c85c91de4d6ba2467
mmsesay <maej_01@hotmail.com>
2021-12-01 16:06:26 +0000

    Add: species data into the data.sql file

--- data.sql
+++ data.sql
@@ -67,3 +67,6 @@ VALUES ('Dean Winchester', 14);
 
 INSERT INTO owners (full_name, age)
 VALUES ('Jodie Whittaker', 38);
+
+INSERT INTO species (name) VALUES ('Pokemon');
+INSERT INTO species (name) VALUES ('Digimon');
