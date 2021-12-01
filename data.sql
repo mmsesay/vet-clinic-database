586c2fa48023abb47cd27cadc41b41738340e4bd
mmsesay <maej_01@hotmail.com>
2021-12-01 13:25:54 +0000

    Add query to alter new column

--- data.sql
+++ data.sql
@@ -19,3 +19,34 @@ INSERT INTO animals(
   	id, name, date_of_birth,
   	escape_attempt, neutered, weight_kg
   	) VALUES(4, 'Devimon', '2017-05-12', 5, true, 11);
+
+INSERT INTO animals(
+  	id, name, date_of_birth,
+  	escape_attempt, neutered, weight_kg
+  	) VALUES(5, 'Charmander', '2020-02-08', 0, false, -11);
+
+INSERT INTO animals(
+  	id, name, date_of_birth,
+  	escape_attempt, neutered, weight_kg
+  	) VALUES(6, 'Plantmon', '2022-11-15', 2, true, -5);
+
+INSERT INTO animals(
+   	id, name, date_of_birth,
+   	escape_attempt, neutered, weight_kg
+   	) VALUES(7, 'Squirtle', '1993-04-02', 3, false, -12.13);
+
+INSERT INTO animals(
+   	id, name, date_of_birth,
+   	escape_attempt, neutered, weight_kg
+   	) VALUES(8, 'Angemon', '2005-06-12', 1, true, -45);
+
+INSERT INTO animals(
+	id, name, date_of_birth,
+	escape_attempt, neutered, weight_kg
+	) VALUES(9, 'Boarmon', '2005-07-07', 7, true, 20.4);
+
+INSERT INTO animals(
+	id, name, date_of_birth,
+	escape_attempt, neutered, weight_kg
+	) VALUES(10, 'Blossom', '1998-08-13', 3, true, 17);
+
