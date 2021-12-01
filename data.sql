46d9dfa046f5133f173947e370458105843560d1
mmsesay <maej_01@hotmail.com>
2021-12-01 16:05:01 +0000

    Add: owners data into the data.sql file

--- data.sql
+++ data.sql
@@ -50,3 +50,20 @@ INSERT INTO animals(
 	escape_attempt, neutered, weight_kg
 	) VALUES(10, 'Blossom', '1998-08-13', 3, true, 17);
 
+INSERT INTO owners (full_name, age)
+VALUES ('Sam Smith', 34);
+
+INSERT INTO owners (full_name, age)
+VALUES ('Jennifer Orwell', 19);
+
+INSERT INTO owners (full_name, age)
+VALUES ('Bob', 45);
+
+INSERT INTO owners (full_name, age)
+VALUES ('Melody Pond', 77);
+
+INSERT INTO owners (full_name, age)
+VALUES ('Dean Winchester', 14);
+
+INSERT INTO owners (full_name, age)
+VALUES ('Jodie Whittaker', 38);
