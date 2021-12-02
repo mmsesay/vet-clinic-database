b84e41aa2c8291d2be467fa4c5a77106cc359905
mmsesay <maej_01@hotmail.com>
2021-12-02 14:45:51 +0000

    Refactor animals input data

--- data.sql
+++ data.sql
@@ -3,70 +3,25 @@
 INSERT INTO animals(
   	id, name, date_of_birth,
   	escape_attempt, neutered, weight_kg
-  	) VALUES(1, 'Agumon', '2020-02-03', 0, true, 10.23);
-
-INSERT INTO animals(
-  	id, name, date_of_birth,
-  	escape_attempt, neutered, weight_kg
-  	) VALUES(2, 'Gabumon', '2018-11-15', 2, true, 8);
-
-INSERT INTO animals(
-  	id, name, date_of_birth,
-  	escape_attempt, neutered, weight_kg
-  	) VALUES(3, 'Pikachu', '2021-01-7', 1, false, 15.04);
-	
-INSERT INTO animals(
-  	id, name, date_of_birth,
-  	escape_attempt, neutered, weight_kg
-  	) VALUES(4, 'Devimon', '2017-05-12', 5, true, 11);
-
-INSERT INTO animals(
-  	id, name, date_of_birth,
-  	escape_attempt, neutered, weight_kg
-  	) VALUES(5, 'Charmander', '2020-02-08', 0, false, -11);
-
-INSERT INTO animals(
-  	id, name, date_of_birth,
-  	escape_attempt, neutered, weight_kg
-  	) VALUES(6, 'Plantmon', '2022-11-15', 2, true, -5);
-
-INSERT INTO animals(
-   	id, name, date_of_birth,
-   	escape_attempt, neutered, weight_kg
-   	) VALUES(7, 'Squirtle', '1993-04-02', 3, false, -12.13);
-
-INSERT INTO animals(
-   	id, name, date_of_birth,
-   	escape_attempt, neutered, weight_kg
-   	) VALUES(8, 'Angemon', '2005-06-12', 1, true, -45);
-
-INSERT INTO animals(
-	id, name, date_of_birth,
-	escape_attempt, neutered, weight_kg
-	) VALUES(9, 'Boarmon', '2005-07-07', 7, true, 20.4);
-
-INSERT INTO animals(
-	id, name, date_of_birth,
-	escape_attempt, neutered, weight_kg
-	) VALUES(10, 'Blossom', '1998-08-13', 3, true, 17);
+) 
+VALUES(1, 'Agumon', '2020-02-03', 0, true, 10.23);
+	(2, 'Gabumon', '2018-11-15', 2, true, 8);
+	(3, 'Pikachu', '2021-01-7', 1, false, 15.04);
+	(4, 'Devimon', '2017-05-12', 5, true, 11);
+	(5, 'Charmander', '2020-02-08', 0, false, -11);
+	(6, 'Plantmon', '2022-11-15', 2, true, -5);
+	(7, 'Squirtle', '1993-04-02', 3, false, -12.13);
+	(8, 'Angemon', '2005-06-12', 1, true, -45);
+	(9, 'Boarmon', '2005-07-07', 7, true, 20.4);
+	10, 'Blossom', '1998-08-13', 3, true, 17);
 
 INSERT INTO owners (full_name, age)
-VALUES ('Sam Smith', 34);
-
-INSERT INTO owners (full_name, age)
-VALUES ('Jennifer Orwell', 19);
-
-INSERT INTO owners (full_name, age)
-VALUES ('Bob', 45);
-
-INSERT INTO owners (full_name, age)
-VALUES ('Melody Pond', 77);
-
-INSERT INTO owners (full_name, age)
-VALUES ('Dean Winchester', 14);
-
-INSERT INTO owners (full_name, age)
-VALUES ('Jodie Whittaker', 38);
+VALUES ('Sam Smith', 34),
+		('Jennifer Orwell', 19),
+		('Bob', 45),
+		('Melody Pond', 77),
+		('Dean Winchester', 14),
+		('Jodie Whittaker', 38);
 
 INSERT INTO species (name) VALUES ('Pokemon');
 INSERT INTO species (name) VALUES ('Digimon');
